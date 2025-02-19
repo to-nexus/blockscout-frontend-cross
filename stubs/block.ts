842a4b82e88f7a99bf4e5880c9024f52bdcf21ba
Owen <owen@to.nexus>
2025-02-19 10:58:18 +0900

    add stub type

--- stubs/block.ts
+++ stubs/block.ts
@@ -35,6 +35,7 @@ export const BLOCK: Block = {
   transaction_fees: '19241635547777613',
   type: 'block',
   uncles_hashes: [],
+  confirmed_validator: 0
 };
 
 const BLOCK_EPOCH_REWARD: BlockEpochElectionReward = {

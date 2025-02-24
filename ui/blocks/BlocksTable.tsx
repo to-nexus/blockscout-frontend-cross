0261c5a55ca70ee40ad7a7169b1ff6d409215ccd
Owen <owen@to.nexus>
2025-02-24 10:56:12 +0900

    fix

--- ui/blocks/BlocksTable.tsx
+++ ui/blocks/BlocksTable.tsx
@@ -47,8 +47,10 @@ const BlocksTable = ({ data, isLoading, top, page, showSocketInfo, socketInfoNum
             <Th width="120px">Size, bytes</Th>
             { !config.UI.views.block.hiddenFields?.miner &&
             <Th width={ `${ VALIDATOR_COL_WEIGHT / widthBase * 100 }%` } minW="160px">{ capitalize(getNetworkValidatorTitle()) }</Th> }
-            <Th width="64px" isNumeric>Txs</Th>
-            <Th width={ `${ GAS_COL_WEIGHT / widthBase * 100 }%` }>Gas used</Th>
+            { !config.UI.views.block.hiddenFields?.confirmed_validator_count &&
+            <Th width={ `${ VALIDATOR_COL_WEIGHT / widthBase * 100 }%` } minW="160px">Confirmed Validators</Th> }
+            <Th width="100px" isNumeric>Txs</Th>
+            <Th width="100px">Gas used</Th>
             { !isRollup && !config.UI.views.block.hiddenFields?.total_reward &&
               <Th width={ `${ REWARD_COL_WEIGHT / widthBase * 100 }%` }>Reward { currencyUnits.ether }</Th> }
             { !isRollup && !config.UI.views.block.hiddenFields?.burnt_fees &&
